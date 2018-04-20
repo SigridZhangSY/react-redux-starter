755d4ec441ba197a3a7b4406ecd236efdeb86dfd
syzhang <sigridzhang0603@163.com>
2018-04-20 13:43:24 +0800

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,19 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+         presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.css$/, // Only .css files
+        loader: 'style!css' // Run both loaders
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
